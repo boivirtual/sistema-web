f25d4b896b4c6d6e72d1a586ceb7797c14122562
boivirtual <boivirtual@agrolandes.com.br>
2026-06-16 08:43:26 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -398,8 +398,8 @@ $data_sistema = date("Y-m-d");
 
                                         <div class="form-group col-md-3">
                                             <label for="codigo_conta" class="control-label"><span class="required">*</span> Conta Contábil</label>
-                                            <select class="form-control" id="codigo_conta" name="codigo_conta" title="Selecione...">
-                                                <option value="0000000" disabled>...</option>
+                                            <select class="form-control" id="codigo_conta" name="codigo_conta">
+                                                <option value="0000000" disabled selected>...</option>
                                                 <?php while ($registro_pcontas = mysqli_fetch_object($plano_contas)) {
                                                     if ($registro_pcontas->tbl_plano_contas_nivel == 1) {
                                                         echo "<option value='{$registro_pcontas->tbl_plano_contas_codigo_id}' disabled style='color:#777; font-weight:600;'>" .
