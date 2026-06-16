e525fa906ad52a4b53ec2f36a3d93967006d1b5c
boivirtual <boivirtual@agrolandes.com.br>
2026-06-16 10:55:52 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -1983,7 +1983,7 @@ $data_sistema = date("Y-m-d");
         html += '<td><input type="text" class="form-control rat-perc" placeholder="0,00%" name="rat2_perc[]" readonly' +
                 ' style="height:30px;font-size:13px;text-align:right;background:#f9f9f9;color:#555;"></td>';
         html += '<td style="text-align:center;"><button type="button" class="btn btn-danger btn-xs"' +
-                ' onclick="excluirLinhaRateio(this)" title="Remover"><i class="fas fa-trash"></i></button></td>';
+                ' onclick="excluirLinhaRateio(this)" title="Remover" style="background:transparent;border:none;color:#2980b9;padding:2px 6px;"><i class="fas fa-trash"></i></button></td>';
         html += '</tr>';
 
         // Insere antes da linha de totais
