f0075ecacf00c19a391f65c75cd4cc0f5f2ba365
boivirtual <boivirtual@agrolandes.com.br>
2026-06-18 11:23:18 -0300

    Ajuste: verificar_doc_ctp.php

---
+++ verificar_doc_ctp.php
@@ -0,0 +1,24 @@
+<?php
+include "conecta_mysql.inc";
+
+$numero_doc  = isset($_POST['numero_doc'])  ? mysqli_real_escape_string($conector, trim($_POST['numero_doc']))  : '';
+$codigo_for  = isset($_POST['codigo_for'])  ? mysqli_real_escape_string($conector, trim($_POST['codigo_for']))  : '';
+
+if ($numero_doc === '' || $codigo_for === '' || $codigo_for === '999999999') {
+    echo '0';
+    exit;
+}
+
+$sql = "SELECT COUNT(*) as total FROM contas_pagar
+        WHERE ctp_numero_doc = '$numero_doc'
+          AND ctp_codigo_fornecedor = '$codigo_for'
+          AND ctp_lixeira = 0";
+
+$result = mysqli_query($conector, $sql);
+if (!$result) {
+    echo '0';
+    exit;
+}
+
+$row = mysqli_fetch_assoc($result);
+echo intval($row['total']) > 0 ? '1' : '0';
