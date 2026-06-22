e921615c2d9704ddb5ac4a9ac971e7130d9d83cc
boivirtual <boivirtual@agrolandes.com.br>
2026-06-22 08:57:56 -0300

    Ajuste: opcoes_menu.php

--- opcoes_menu.php
+++ opcoes_menu.php
@@ -103,7 +103,7 @@
               <span class="menu-arrow arrow_carrot-right"></span>
             </a>
             <ul class="sub">
-              <li id="opc502"><a class="" href="form_contas_pagar.php">Contas a Pagar</a></li>
+              <li id="opc502"><a class="" href="form_contas_pagar.php?reset=1">Contas a Pagar</a></li>
               <li id="opc502d"><a style="color:#666666" href="#">Contas a Pagar</a></li>
               <li id="opc504"><a class="" href="form_contas_receber.php">Contas a Receber</a></li>
               <li id="opc504d"><a style="color:#666666" href="#">Contas a Receber</a></li>
