f67f8c86d8c54a1f4370d914db1772efc49a0e6d
boivirtual <boivirtual@agrolandes.com.br>
2026-06-23 09:45:33 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -538,7 +538,7 @@ $data_sistema = date("Y-m-d");
                                                 <div class="form-group col-md-3">
                                                     <label for="intervalo" class="control-label"><span class="required">*</span> Intervalo (dias)</label>
                                                     <input type="number" class="form-control" id="intervalo" name="intervalo"
-                                                           value="30" min="1" onchange="recalcularDatas()">
+                                                           value="30" min="1" onchange="onIntervaloChange()">
                                                 </div>
                                                 <div class="form-group col-md-4">
                                                     <label for="primeiro_vencimento" class="control-label"><span class="required">*</span> 1º Vencimento</label>
