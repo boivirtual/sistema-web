b925bfe60a74240c05b73a09d5f2956ba94085fb
boivirtual <boivirtual@agrolandes.com.br>
2026-06-24 15:03:17 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -2101,7 +2101,7 @@ $data_sistema = date("Y-m-d");
             var $bs = $s.closest('.bootstrap-select');
             $bs.css({ 'width': '100%', 'display': 'block' });
             $bs.find('button.dropdown-toggle').css({ 'height': '30px', 'font-size': '13px', 'padding': '4px 8px', 'width': '100%', 'overflow': 'hidden', 'text-overflow': 'ellipsis', 'white-space': 'nowrap' });
-            $bs.find('.dropdown-menu').css({ 'min-width': '220px', 'width': 'auto' });
+            $bs.find('.dropdown-menu').css({ 'min-width': '280px', 'width': 'auto' });
         });
 
         $('#tr_local_input').hide();
