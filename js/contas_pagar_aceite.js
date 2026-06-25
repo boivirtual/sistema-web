ba2391db625a9696e801f2ded579b5748b95a263
boivirtual <boivirtual@agrolandes.com.br>
2026-06-25 14:11:12 -0300

    Ajuste: js/contas_pagar_aceite.js

--- js/contas_pagar_aceite.js
+++ js/contas_pagar_aceite.js
@@ -636,7 +636,7 @@ function _eratGerarLinha(ln, showLocal, showCC) {
         tr += '<td style="vertical-align:middle;padding:4px 8px;overflow:hidden;white-space:nowrap;">' +
             '<span class="lbl-parcela">' + ccNome + '</span>' +
             ' <a href="#" onclick="eratEditarCC(this);return false;"' +
-            ' style="color:#337ab7;font-size:11px;margin-left:4px;" data-toggle="tooltip" title="Editar Centro de Custo"><i class="far fa-edit"></i></a>' +
+            ' style="color:#337ab7;font-size:11px;margin-left:4px;" data-toggle="tooltip" title="Selecionar Centro de Custo"><i class="fas fa-pen"></i></a>' +
             '<input type="hidden" class="erat-cc-id"   value="' + ccId   + '">' +
             '<input type="hidden" class="erat-cc-nome" value="' + ccNome + '">' +
             '</td>';
