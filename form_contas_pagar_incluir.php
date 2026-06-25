b7e5316ef1f69b5b0d069159d57b1c7feb83772d
boivirtual <boivirtual@agrolandes.com.br>
2026-06-25 07:01:02 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -2088,9 +2088,6 @@ $data_sistema = date("Y-m-d");
             html += '</tr>';
         });
 
-        html += '<tr id="tr_editar_locais"><td style="padding:4px 8px;">' +
-                '<a href="#" onclick="editarLocaisRateio();return false;" style="color:#aaa;font-size:13px;">' +
-                '<i class="far fa-edit"></i> Selecionar Locais</a></td><td colspan="5"></td></tr>';
         html += '</tbody></table>';
         $('#linhas_rateio').html(html);
 
@@ -2173,9 +2170,6 @@ $data_sistema = date("Y-m-d");
             html += '</tr>';
         });
 
-        html += '<tr id="tr_editar_locais"><td style="padding:4px 8px;">' +
-                '<a href="#" onclick="editarLocaisRateio();return false;" style="color:#aaa;font-size:13px;">' +
-                '<i class="far fa-edit"></i> Selecionar Locais</a></td><td colspan="5"></td></tr>';
         html += '</tbody></table>';
         $('#linhas_rateio').html(html);
 
