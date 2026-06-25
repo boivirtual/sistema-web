38f45c5b361bfd57649bb7cdaa7eee3704bc002e
boivirtual <boivirtual@agrolandes.com.br>
2026-06-25 08:38:45 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -2359,7 +2359,7 @@ $data_sistema = date("Y-m-d");
             '<span class="lbl-parcela">' + ccNome + '</span></td>' +
             '<td style="vertical-align:middle;padding:4px 8px;">' +
             '<select class="selectpicker" id="' + selectId + '" multiple data-live-search="true" data-size="8" data-width="100%">' + optionsConta + '</select></td>' +
-            '<td style="vertical-align:middle;padding:4px 8px;">' +
+            '<td style="vertical-align:middle;padding:4px 8px;white-space:nowrap;">' +
             '<button type="button" class="btn btn-primary" onclick="confirmarContaDoCC(\'' + localId + '\',\'' + ccId + '\',\'' + localNomeJs + '\',\'' + ccNomeJs + '\')">Confirmar</button>' +
             ' <button type="button" class="btn btn-default" onclick="fecharEdicaoConta(\'' + localId + '\',\'' + ccId + '\')">Fechar</button></td>' +
             '<td colspan="2"></td></tr>';
