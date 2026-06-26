18724d90692d4fc989c6a5a03a3c3c5513824086
boivirtual <boivirtual@agrolandes.com.br>
2026-06-26 11:02:55 -0300

    Ajuste: js/contas_pagar_aceite.js

--- js/contas_pagar_aceite.js
+++ js/contas_pagar_aceite.js
@@ -818,11 +818,12 @@ function eratEditarLocal(link) {
     try { $('#modal_editar_rateio [data-toggle="tooltip"]').tooltip('destroy'); } catch (e) {}
     $('.tooltip').remove();
     $td.data('orig-html', $td.html()).html(
+        '<div style="display:flex;align-items:center;gap:4px;width:100%;">' +
+        '<div style="flex:1;min-width:0;">' +
         '<select id="' + selId + '" class="selectpicker" multiple data-live-search="true"' +
         ' data-width="100%" data-container="body" title="Selecione o local...">' +
-        optLocal + '</select>' +
-        '<div style="margin-top:4px;white-space:nowrap;">' +
-        '<button type="button" class="btn btn-primary btn-xs" onclick="eratConfirmarLocal(this)">Confirmar</button> ' +
+        optLocal + '</select></div>' +
+        '<button type="button" class="btn btn-primary btn-xs" onclick="eratConfirmarLocal(this)">Confirmar</button>' +
         '<button type="button" class="btn btn-default btn-xs" onclick="eratCancelarEdicao(this)">Fechar</button>' +
         '</div>'
     );
