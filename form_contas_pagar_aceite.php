8dd27088f2c705afa23954964e4b2d548fe226c1
boivirtual <boivirtual@agrolandes.com.br>
2026-06-26 11:45:01 -0300

    Ajuste: form_contas_pagar_aceite.php

--- form_contas_pagar_aceite.php
+++ form_contas_pagar_aceite.php
@@ -413,11 +413,11 @@ var _eratContas = <?php echo json_encode($arr_conta_rat_js, JSON_UNESCAPED_UNICO
                 <div style="overflow-x:auto;">
                     <table class="tbl-parcelas" id="tbl_erat">
                         <colgroup>
-                            <col style="width:20%">
-                            <col style="width:18%">
-                            <col style="width:32%">
-                            <col style="width:16%">
+                            <col style="width:26%">
+                            <col style="width:22%">
+                            <col style="width:28%">
                             <col style="width:14%">
+                            <col style="width:10%">
                         </colgroup>
                         <thead>
                             <tr>
