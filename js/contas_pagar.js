b734de90184c9e189122289a44930ffdc788c65b
boivirtual <boivirtual@agrolandes.com.br>
2026-06-29 14:59:21 -0300

    Ajuste: js/contas_pagar.js

--- js/contas_pagar.js
+++ js/contas_pagar.js
@@ -50,7 +50,7 @@ function toggleRateio(id) {
                 '</div>' +
                 '<div class="modal-body" style="overflow-x:auto;padding:12px 16px;">' + corpo + '</div>' +
                 '<div class="modal-footer">' +
-                '<button class="btn btn-primary" type="button" style="float:left;" onclick="$(\'#modal_rateio_ctp_dyn\').modal(\'hide\');abrirEditarRateio(' + id + ');"><i class="fas fa-edit"></i> Editar</button>' +
+                '<button class="btn btn-primary" type="button" style="float:left;" onclick="$(\'#modal_rateio_ctp_dyn\').modal(\'hide\');abrirEditarRateio(' + id + ');">Editar</button>' +
                 '<button class="btn btn-default" type="button" data-dismiss="modal">Fechar</button>' +
                 '</div>' +
                 '</div></div></div>';
