893419126b4e16b9df21573745d61e7e6495458b
boivirtual <boivirtual@agrolandes.com.br>
2026-06-29 14:13:27 -0300

    Ajuste: modal_anexos.php

--- modal_anexos.php
+++ modal_anexos.php
@@ -132,8 +132,9 @@ function abrirModalAnexos(numero_doc, codigo_fornecedor, ctp_id, doc_display, ab
         _ctpAnexosHandler = true;
     }
 
+    _ctpAnexosAutoInputs = !!abrirInputs;
     $('#modal_anexos_doc').text('— Documento: ' + doc_display);
-    if (abrirInputs) { _maAbrirInputs(); } else { _maResetar(); }
+    _maResetar();
     $('#modal_anexos').modal('show');
     _carregarAnexos();
 }
