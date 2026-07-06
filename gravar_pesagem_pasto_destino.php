b5fdc8aab12a09e7debea1f4975dafaf51963353
boivirtual <boivirtual@agrolandes.com.br>
2026-07-06 11:02:15 -0300

    Ajuste: gravar_pesagem_pasto_destino.php

--- gravar_pesagem_pasto_destino.php
+++ gravar_pesagem_pasto_destino.php
@@ -200,7 +200,8 @@
 			tbl_pesagem_pasto,
 			tbl_pesagem_categoria,
 			tbl_pesagem_sexo,
-			tbl_pesagem_codigo_movimentacao
+			tbl_pesagem_codigo_movimentacao,
+			tbl_pesagem_origem
 	        ) VALUES (
 	        'L',
 	        '$data_pesagem',
