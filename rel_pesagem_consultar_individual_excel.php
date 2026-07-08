922f05ad9e60d24433dff918c880da6291daa1f3
boivirtual <boivirtual@agrolandes.com.br>
2026-07-08 08:48:09 -0300

    Ajuste: rel_pesagem_consultar_individual_excel.php

--- rel_pesagem_consultar_individual_excel.php
+++ rel_pesagem_consultar_individual_excel.php
@@ -82,7 +82,7 @@ if ($num_rows != 0) {
     $data_pesagem = new DateTime($reg_pesagem->tbl_pesagem_data);
     $data_pesagem_edi = $data_pesagem->format('d/m/Y');
 
-    $nome_inclusao = $reg_pesagem->tbl_pesagem_incluido_por;
+    $nome_inclusao = corrigir_utf8($reg_pesagem->tbl_pesagem_incluido_por);
     $data_inclusao = new DateTime($reg_pesagem->tbl_pesagem_incluido_em);
     $incluido_por = $nome_inclusao . ' em ' . $data_inclusao->format('d/m/Y');
 }
