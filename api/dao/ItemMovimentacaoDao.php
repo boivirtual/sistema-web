0bcab92e5b3d796f29939c7d683a03e16a84636d
boivirtual <boivirtual@agrolandes.com.br>
2026-07-10 10:57:02 -0300

    Ajuste: api/dao/ItemMovimentacaoDao.php

--- api/dao/ItemMovimentacaoDao.php
+++ api/dao/ItemMovimentacaoDao.php
@@ -4,7 +4,7 @@ class ItemMovimentacaoDao{
     private $con;
 
     public function __construct($banco){
-        include_once __DIR__ . "/../../conecta_mysql_credenciais.inc";
+        require __DIR__ . "/../../conecta_mysql_credenciais.inc";
         $this->con = mysqli_connect($servidor, $usuario_bd, $senha_bd, $banco);
         $this->systemDate = date('Y-m-d H:i:s');
     }
