fe762f1aafbefdeec421140359f89744606bb7fe
boivirtual <boivirtual@agrolandes.com.br>
2026-07-15 10:01:46 -0300

    Ajuste: form_lista_analise_recebimento_rel.php

--- form_lista_analise_recebimento_rel.php
+++ form_lista_analise_recebimento_rel.php
@@ -79,9 +79,7 @@
     $wcc = '';
 
     if ($codigo_cc!='') {
-        $wcc = " AND ctr_codigo_c_custo IN(";
-        $wcc.= $cc;
-        $wcc.= ")";
+        $wcc = " AND (ctr_codigo_c_custo IN($cc) OR (ctr_codigo_c_custo IS NULL AND ctr_id IN (SELECT rc_ctr_id FROM tbl_ctr_rateio WHERE rc_codigo_cc IN ($cc))))";
     }
 
     $fazendas= array();
