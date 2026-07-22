56e422e817713db586fa6dae9cbb025666a28be4
boivirtual <boivirtual@agrolandes.com.br>
2026-07-22 09:12:47 -0300

    Ajuste: rel_analise_recebimentos_excel.php

--- rel_analise_recebimentos_excel.php
+++ rel_analise_recebimentos_excel.php
@@ -149,7 +149,7 @@ $fazendas = substr($fazendas,0, -1);
 $wfazendas = '';
 
 if ($codigo_fazenda!='') {
-    $wfazendas = " AND (ctr_codigo_fazenda IN($fazendas) OR (ctr_codigo_fazenda IS NULL AND ctr_id IN (SELECT rc_ctr_id FROM tbl_ctr_rateio WHERE rc_codigo_local IN ($fazendas))))";
+    $wfazendas = " AND (ctr_codigo_fazenda IN($fazendas) OR " . condicao_rateio_ou_grupo_ctr('ctr_codigo_fazenda', 'rc_codigo_local', $fazendas) . ")";
 }
 
 //$conta_inicio=$codigo_conta;    
