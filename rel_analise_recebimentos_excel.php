40f441435ae4f133d36cea73353de586718c96de
boivirtual <boivirtual@agrolandes.com.br>
2026-07-22 09:13:56 -0300

    Ajuste: rel_analise_recebimentos_excel.php

--- rel_analise_recebimentos_excel.php
+++ rel_analise_recebimentos_excel.php
@@ -1337,7 +1337,7 @@ function montar_fatias_conta_rateio_ctr($conector, $ctr_id, $cod_conta_header, $
 
 function ler_notas($conector, $data_sistema,$tipo_data,$data_inicial,$data_final,$conta_inicio,$wcc,$wcliente,$wfazendas){
 
-    $wconta_notas = " AND (ctr_codigo_conta='$conta_inicio' OR (ctr_codigo_conta IS NULL AND ctr_id IN (SELECT rc_ctr_id FROM tbl_ctr_rateio WHERE rc_codigo_conta='$conta_inicio')))";
+    $wconta_notas = " AND (ctr_codigo_conta='$conta_inicio' OR " . condicao_rateio_ou_grupo_ctr('ctr_codigo_conta', 'rc_codigo_conta', "'$conta_inicio'") . ")";
 
     if ($tipo_data=="E"){
         $contas_rec = mysqli_query($conector, "SELECT * FROM contas_receber
