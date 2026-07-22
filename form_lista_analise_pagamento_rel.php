355145ced636f2664259b215ac44b19f3a9d393d
boivirtual <boivirtual@agrolandes.com.br>
2026-07-22 08:29:15 -0300

    Ajuste: form_lista_analise_pagamento_rel.php

--- form_lista_analise_pagamento_rel.php
+++ form_lista_analise_pagamento_rel.php
@@ -515,7 +515,7 @@
                     }
                 }
 
-                $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer);
+                $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao);
 
                 if (count($fatias) == 0) {
                     $total_sem_conta = $total_sem_conta + $total_pagar;
@@ -677,7 +677,7 @@
                 }
             }
 
-            $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer);
+            $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao);
 
             if (count($fatias) == 0) {
                 $total_sem_conta = $total_sem_conta + $total_pagar;
