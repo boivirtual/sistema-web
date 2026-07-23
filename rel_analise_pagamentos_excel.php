2b14c4bd63fcee67feb9fb5a64c2e822344ae25c
boivirtual <boivirtual@agrolandes.com.br>
2026-07-23 09:42:39 -0300

    Ajuste: rel_analise_pagamentos_excel.php

--- rel_analise_pagamentos_excel.php
+++ rel_analise_pagamentos_excel.php
@@ -481,7 +481,7 @@ $linha=4;
                     }
                 }
 
-                $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao, $registro_contas_pagar->ctp_numero_doc, $registro_contas_pagar->ctp_codigo_fornecedor);
+                $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao, $registro_contas_pagar->ctp_numero_doc, $registro_contas_pagar->ctp_codigo_fornecedor, $registro_contas_pagar->ctp_incluido_em);
 
                 if (count($fatias) == 0) {
                     $total_sem_conta = $total_sem_conta + $total_pagar;
@@ -650,7 +650,7 @@ $linha=4;
                 }
             }
 
-            $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao, $registro_contas_pagar->ctp_numero_doc, $registro_contas_pagar->ctp_codigo_fornecedor);
+            $fatias = montar_fatias_conta_rateio($conector, $ctp_id, $registro_contas_pagar->ctp_codigo_conta, $total_pagar, $valor_pago, $total_vencidas, $total_avencer, $registro_contas_pagar->ctp_grupo_repeticao, $registro_contas_pagar->ctp_numero_doc, $registro_contas_pagar->ctp_codigo_fornecedor, $registro_contas_pagar->ctp_incluido_em);
 
             if (count($fatias) == 0) {
                 $total_sem_conta = $total_sem_conta + $total_pagar;
