7ec02ddd96ae5723859b5dfe0bc2e6fcbc9f6607
boivirtual <boivirtual@agrolandes.com.br>
2026-08-05 13:31:21 -0300

    Ajuste: form_contas_pagar_incluir.php

--- form_contas_pagar_incluir.php
+++ form_contas_pagar_incluir.php
@@ -545,7 +545,7 @@ $data_sistema = date("Y-m-d");
                                         </div>
 
                                         <!-- Bloco À Vista: Vencimento | Banco | Tipo Doc | Pago -->
-                                        <div id="bloco_avista" class="col-md-9" style="padding: 0;">
+                                        <div id="bloco_avista" class="col-md-9" style="padding: 0; display: none;">
                                             <div class="row" style="margin: 0;">
                                                 <div class="form-group col-md-3">
                                                     <label for="data_vencimento" class="control-label"><span class="required">*</span> Vencimento</label>
