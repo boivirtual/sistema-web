5d0132a2e006d18a94613769797ca7f1e2be7b5c
boivirtual <boivirtual@agrolandes.com.br>
2026-08-05 09:00:30 -0300

    Ajuste: form_lista_analise_previsto_realizado_rel.php

--- form_lista_analise_previsto_realizado_rel.php
+++ form_lista_analise_previsto_realizado_rel.php
@@ -274,17 +274,13 @@
     $wlocal_pag = '';
 
     if ($codigo_fazendas!='') {
-        $wlocal_pag = " AND ctp_codigo_fazenda IN(";
-        $wlocal_pag.= $fazendas;
-        $wlocal_pag.= ")";
+        $wlocal_pag = " AND (ctp_codigo_fazenda IN($fazendas) OR " . condicao_rateio_ou_grupo('ctp_codigo_fazenda', 'rc_codigo_local', $fazendas) . ")";
     }
 
     $wlocal_rec = '';
 
     if ($codigo_fazendas!='') {
-        $wlocal_rec = " AND ctr_codigo_fazenda IN(";
-        $wlocal_rec.= $fazendas;
-        $wlocal_rec.= ")";
+        $wlocal_rec = " AND (ctr_codigo_fazenda IN($fazendas) OR " . condicao_rateio_ou_grupo_ctr('ctr_codigo_fazenda', 'rc_codigo_local', $fazendas) . ")";
     }
 
     $wlocal_previsao = '';
