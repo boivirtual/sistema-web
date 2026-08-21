81368c067bdbc8b38cbacbffe7aa37130eb18480
boivirtual <boivirtual@agrolandes.com.br>
2026-08-21 10:39:08 -0300

    Ajuste: js/pesagem_lote.js

--- js/pesagem_lote.js
+++ js/pesagem_lote.js
@@ -1388,6 +1388,8 @@ function gravar_pasto_grupo_pesagem(pasto, grupo, id_pesagem){
 }
 
 // Grava peso digitado na lista do excel
+var _pesagemItemEmAndamento = {};
+
 function gravar_item_alterado_editar_pesagem(
     codigo_id,
     item,
@@ -1396,6 +1398,7 @@ function gravar_item_alterado_editar_pesagem(
     excluir,
     grupo
 ) {
+    if (_pesagemItemEmAndamento[codigo_id]) return;
 
     var total_peso = 0;
     var animais_pesados = 0;
