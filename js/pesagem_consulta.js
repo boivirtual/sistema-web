ead6f22e8456c1c15796d5eccada38cad69b5445
boivirtual <boivirtual@agrolandes.com.br>
2026-08-21 10:27:41 -0300

    Ajuste: js/pesagem_consulta.js

--- js/pesagem_consulta.js
+++ js/pesagem_consulta.js
@@ -5604,11 +5604,14 @@ function gravar_pesagem(opcao) {
     $("#array_itens").val(grupo_itens);
     var dados = $("#form_gravar_pesagem").serialize();
 
+    _gravarPesagemEmAndamento = true;
+
     $.ajax({
         type: "POST",
         url: "gravar_pesagem_individual.php",
         data: dados,
         success: function (data) {
+            _gravarPesagemEmAndamento = false;
             if (data.error) {
                 $("#mensagem_erro").modal();
                 $("#mensagem_erro .modal-body").html(data.message);
@@ -5623,6 +5626,9 @@ function gravar_pesagem(opcao) {
                     monta_lista_editar_online();
                 }
             }
+        },
+        error: function () {
+            _gravarPesagemEmAndamento = false;
         },
     });
 }
