f0589292a3ef28b1c099481cd89ab3041ab3624f
boivirtual <boivirtual@agrolandes.com.br>
2026-08-27 08:23:31 -0300

    Ajuste: js/agenda_protocolos.js

--- js/agenda_protocolos.js
+++ js/agenda_protocolos.js
@@ -322,12 +322,7 @@ $(document).on('hidden.bs.modal', '.modal', function(){
         }
     });
 
-    $('.fc-daygrid-day, .fc-timegrid-col').each(function(){
-        if ($(this).data('agendaTooltipCliqueVisivel')) {
-            $(this).tooltip('hide');
-            $(this).data('agendaTooltipCliqueVisivel', false);
-        }
-    });
+    esconderTooltipsCliqueIncluir();
 });
 
 function aplicarTooltipsEventos(){
