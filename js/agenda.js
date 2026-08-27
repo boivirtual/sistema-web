f25449bb56082acf9ea4444c47137156ce058387
boivirtual <boivirtual@agrolandes.com.br>
2026-08-27 08:19:11 -0300

    Ajuste: js/agenda.js

--- js/agenda.js
+++ js/agenda.js
@@ -512,8 +512,6 @@ function iniciarCalendarioAgenda(){
         $('.fc-popover').remove();
     });
 
-    $('#main-content').off('scroll.agendaTooltipClique').on('scroll.agendaTooltipClique', esconderTooltipsCliqueIncluir);
-    $('.agenda-calendar-area').off('scroll.agendaTooltipClique').on('scroll.agendaTooltipClique', esconderTooltipsCliqueIncluir);
 }
 
 function ajustarAlturaCalendario(){
