62d6509211c8999a06e5b3ff765ce2277e48cd54
boivirtual <boivirtual@agrolandes.com.br>
2026-09-01 16:10:58 -0300

    Ajuste: form_lista_animais_matrizes_sem_grupo.php

--- form_lista_animais_matrizes_sem_grupo.php
+++ form_lista_animais_matrizes_sem_grupo.php
@@ -359,24 +359,15 @@
                     $codigo_pai = 0;
                 }
 
-                $tab_pai = mysqli_query($conector, "SELECT * FROM tbl_semem WHERE tbl_semem_codigo_id='$codigo_pai'");
-                $num_rows_pai = mysqli_num_rows($tab_pai);
-
-                if ($num_rows_pai!=0){
-                    $reg = mysqli_fetch_object($tab_pai);
-                    $descricao_pai = $reg->tbl_semem_nome;
+                if (isset($GLOBALS['sg_pai_semem'][$codigo_pai])) {
+                    $descricao_pai = $GLOBALS['sg_pai_semem'][$codigo_pai];
+                }
+                else if (isset($GLOBALS['sg_pai_animal'][$codigo_pai])) {
+                    $reg = $GLOBALS['sg_pai_animal'][$codigo_pai];
+                    $descricao_pai = $reg->tbl_animal_codigo_alfa. ' ' . $reg->tbl_animal_codigo_numerico;
                 }
                 else {
-                    $tab_pai = mysqli_query($conector, "SELECT * FROM tbl_animais WHERE tbl_animal_codigo_id='$codigo_pai'");
-                    $num_rows_pai = mysqli_num_rows($tab_pai);
-
-                    if ($num_rows_pai!=0){
-                        $reg = mysqli_fetch_object($tab_pai);
-                        $descricao_pai = $reg->tbl_animal_codigo_alfa. ' ' . $reg->tbl_animal_codigo_numerico;
-                    }
-                    else {
-                        $descricao_pai = '';
-                    }
+                    $descricao_pai = '';
                 }
 
                 // verifica NatMorto
