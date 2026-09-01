11281ba6ffc2e5cfc33bb3d50c906738305080db
boivirtual <boivirtual@agrolandes.com.br>
2026-09-01 16:12:11 -0300

    Ajuste: form_lista_animais_matrizes_sem_grupo.php

--- form_lista_animais_matrizes_sem_grupo.php
+++ form_lista_animais_matrizes_sem_grupo.php
@@ -424,41 +424,9 @@
                         echo "<td width='16%'>";
                         echo '<select class="form-control grupo_select" name="grupo_select" style="height: 2.1em;" onchange="somar_selecionados()">';
 
-                        echo '<option value="000">...</option>';
+                        // Lista de grupos idêntica para todas as fêmeas — montada uma única vez na pré-carga
+                        echo $GLOBALS['sg_opcoes_grupo'];
 
-                            $grupo_estacao = mysqli_query($conector, "SELECT * FROM tbl_grupo_estacao_monta 
-                                    WHERE tbl_grupo_codigo_estacao_monta ='$id_parametro_estacao' AND 
-                                          tbl_grupo_codigo_local='$local'
-                                    ORDER BY tbl_grupo_id  ASC"); 
-                            $num_grupo = mysqli_num_rows($grupo_estacao);
-
-                            if ($num_grupo!=0) {
-                                while ($reg_grupo = mysqli_fetch_object($grupo_estacao)){
-                                    $codigo_grupo = $reg_grupo->tbl_grupo_id;
-                                    $desc_grupo = $reg_grupo->tbl_grupo_descricao;
-
-                                    $tbl_cobertura = mysqli_query($conector,"SELECT * FROM tbl_cobertura 
-                                        WHERE tbl_cobertura_lixeira=0 AND 
-                                              tbl_cobertura_codigo_grupo='$codigo_grupo' AND 
-                                              tbl_cobertura_codigo_estacao_monta='$id_parametro_estacao' AND 
-                                              tbl_cobertura_codigo_local='$local'"); 
-
-                                    $num_rows_cobertura = mysqli_num_rows($tbl_cobertura);
-
-                                    if ($num_rows_cobertura==0 || $codigo_grupo==999) {
-                                        echo '<option value="'.$codigo_grupo.'">' .$desc_grupo. '</option>';
-                                    }
-                                    else if ($num_rows_cobertura!=0) {
-                                        $reg_cobertura = mysqli_fetch_object($tbl_cobertura);
-                                        $qtd_animais = trim($reg_cobertura->tbl_cobertura_qtd_animais, "0");
-                                        $protocolo_iatf = $reg_cobertura->tbl_cobertura_protocoloiatf;
-
-                                        if ($protocolo_iatf==0) {
-                                            echo '<option value="'.$codigo_grupo.'">' .$desc_grupo. ' - ' . $qtd_animais .' Fêmea(s)</option>';
-                                        }
-                                    }
-                                }
-                            }
                         echo '</select>';
                         echo "</td>";
                     }
