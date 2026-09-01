58c706036278c04a780db204f56156bb2e24acb0
boivirtual <boivirtual@agrolandes.com.br>
2026-09-01 14:17:07 -0300

    Ajuste: form_lista_animais_matrizes.php

--- form_lista_animais_matrizes.php
+++ form_lista_animais_matrizes.php
@@ -2,6 +2,17 @@
     include "conecta_mysql.inc";
     include "valida_sessao.inc";
 
+    // Monta lista de valores para cláusula IN (...) já escapando cada item
+    if (!function_exists('flam_in_list')) {
+        function flam_in_list($conector, $valores) {
+            $itens = array();
+            foreach ($valores as $v) {
+                $itens[] = "'" . mysqli_real_escape_string($conector, $v) . "'";
+            }
+            return implode(',', $itens);
+        }
+    }
+
     $data_hoje = date("Y-m-d");
 
     $tipo_registro = ltrim($_POST['tipo_registro']);
