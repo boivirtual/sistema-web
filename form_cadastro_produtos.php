e09b260501795a5e5560d9a170e74bc1ac138e22
boivirtual <boivirtual@agrolandes.com.br>
2026-09-11 11:03:39 -0300

    Ajuste: form_cadastro_produtos.php

--- form_cadastro_produtos.php
+++ form_cadastro_produtos.php
@@ -175,7 +175,7 @@
                                             <div class="form-group col-md-6 text-right" id="div_ativo" style="display: none;">
                                                 <label class="control-label">&nbsp;</label>
                                                 <div class="checkbox" style="display: inline-block; margin-top: 0;">
-                                                    <label style="padding-left: 20px;"><input type="checkbox" id="ativo" name="ativo" value="S"> Ativo</label>
+                                                    <label><input type="checkbox" id="ativo" name="ativo" value="S"> Ativo</label>
                                                 </div>
                                             </div>
                                         </div>
