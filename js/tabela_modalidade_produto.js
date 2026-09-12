72528bd6e9734ba960d1f8ce69ea8cbdc470f873
boivirtual <boivirtual@agrolandes.com.br>
2026-09-12 18:22:57 -0300

    Ajuste: js/tabela_modalidade_produto.js

--- js/tabela_modalidade_produto.js
+++ js/tabela_modalidade_produto.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     $('#tabela_modalidade_produto').DataTable({
         "responsive": true,
         "paging":   false,
-        "ordering": true,
+        "ordering": false,
         "info":     true,
         "language": {
         "sSearch": "Busca:",
