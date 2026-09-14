4d290c817c07d23de65f4d31db98ac7040b5501e
boivirtual <boivirtual@agrolandes.com.br>
2026-09-14 07:56:46 -0300

    Ajuste: gravar_contas_pagar.php

--- gravar_contas_pagar.php
+++ gravar_contas_pagar.php
@@ -1166,13 +1166,16 @@ ob_start(function($buffer) {
 		// Quando a conta tem rateio, Local/CC/Conta devem permanecer NULL no banco
 		$rateio_existente_ed = !empty($_POST['rateio_existente']) && $_POST['rateio_existente'] === '1';
 
-		// Valor e qtd de parcelas atuais (antes do UPDATE), para decidir se o rateio
-		// de uma conta com parcela única precisa ser recalculado
+		// Valor, qtd de parcelas, doc e fornecedor atuais (antes do UPDATE), para
+		// decidir se o rateio precisa ser recalculado e, se for conta parcelada,
+		// achar as parcelas-irmãs pela identidade que o registro tinha até agora
 		$ctp_id_esc = (int) $ctp_id;
-		$rs_atual   = mysqli_query($conector, "SELECT ctp_valor_parcela, ctp_qtd_parcelas FROM contas_pagar WHERE ctp_id='$ctp_id_esc'");
+		$rs_atual   = mysqli_query($conector, "SELECT ctp_valor_parcela, ctp_qtd_parcelas, ctp_numero_doc, ctp_codigo_fornecedor FROM contas_pagar WHERE ctp_id='$ctp_id_esc'");
 		$reg_atual  = $rs_atual ? mysqli_fetch_object($rs_atual) : null;
-		$vlr_parcela_antigo = $reg_atual ? (float) $reg_atual->ctp_valor_parcela : 0.00;
-		$qtd_parcela_atual  = $reg_atual ? (int)   $reg_atual->ctp_qtd_parcelas  : 1;
+		$vlr_parcela_antigo       = $reg_atual ? (float) $reg_atual->ctp_valor_parcela      : 0.00;
+		$qtd_parcela_atual        = $reg_atual ? (int)   $reg_atual->ctp_qtd_parcelas       : 1;
+		$numero_doc_antigo        = $reg_atual ? $reg_atual->ctp_numero_doc                 : '';
+		$codigo_fornecedor_antigo = $reg_atual ? $reg_atual->ctp_codigo_fornecedor          : '';
 
 		if (empty($_POST['vlr_parcela'])) {
 			$vlr_parcela = 0.00;
