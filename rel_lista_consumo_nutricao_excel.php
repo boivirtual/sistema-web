adc9daa0143719e5bc033918b8f79e95c0d983e5
boivirtual <boivirtual@agrolandes.com.br>
2026-09-20 16:14:07 -0300

    Ajuste: rel_lista_consumo_nutricao_excel.php

--- rel_lista_consumo_nutricao_excel.php
+++ rel_lista_consumo_nutricao_excel.php
@@ -599,6 +599,8 @@ else {
 
         $rs = mysqli_query($conector, $sql);
 
+        $sem_registros = (mysqli_num_rows($rs)==0);
+
         while ($reg_nut = mysqli_fetch_object($rs)){
             $codigo_nutricao_id = $reg_nut->tbl_nutricao_id;
             $codigo_local = $reg_nut->tbl_nutricao_codigo_local;
